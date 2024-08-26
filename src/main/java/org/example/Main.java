729bdcb3b2586b40326b28212a25cbcd869c8735
19Pawel97 <19pawel9772@gmail.com>
2024-08-26 19:29:23 +0200

    DataRepository from master, DataGenerator added + Main updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+
+    static DataRepository dataRepository = new DataRepository();
+
+    static final Object lock1 = new Object();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        DataGenerator dataGenerator = new DataGenerator();
+
+        executor.submit(dataGenerator);
+
+        executor.shutdown();
+
     }
 }
