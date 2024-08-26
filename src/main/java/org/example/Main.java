a7ec316a708aeb01cd93d6d682244e203dccdc23
19Pawel97 <19pawel9772@gmail.com>
2024-08-26 20:00:14 +0200

    Adding dataService, synchronising using Futures, adding everything to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,43 @@
 package org.example;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
     static DataRepository dataRepository = new DataRepository();
 
     static final Object lock1 = new Object();
+    static final Object lock2 = new Object();
 
     public static void main(String[] args) {
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
         DataGenerator dataGenerator = new DataGenerator();
+        DataService dataService = new DataService();
 
-        executor.submit(dataGenerator);
+        Future<?> generatorFuture = executor.submit(dataGenerator);
+        try {
+            generatorFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Future<?> serviceFuture = executor.submit(dataService);
+        try {
+            serviceFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
         executor.shutdown();
 
+        System.out.println(DataService.avg);
     }
 }
